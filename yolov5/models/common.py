4de74d626b29327ea75dd9391a6b3b2315e45f33
Henistein <henriquejesus20@gmail.com>
2022-03-01 00:53:13 +0000

    simple inference

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -1,6 +1,13 @@
 import torch
 import torch.nn as nn
 import pkg_resources as pkg
+import math
+import warnings
+import time
+import numpy as np
+import torchvision
+
+torch.manual_seed(420)
 
 def autopad(k, p=None):  # kernel, padding
   # Pad to 'same'
@@ -8,16 +15,21 @@ def autopad(k, p=None):  # kernel, padding
     p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
   return p
 
-class Conv(nn.Module):
-  # Standard convolution
-  def __init__(self, ch_in, ch_out, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-    super().__init__()
-    self.conv = nn.Conv2d(ch_in, ch_out, k, s, autopad(k, p), groups=g, bias=False)
-    self.bn = nn.BatchNorm2d(ch_out)
-    self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+def make_divisible(x, divisor):
+    # Returns x evenly divisible by divisor
+    return math.ceil(x / divisor) * divisor
 
-  def forward(self, x):
-    return self.act(self.bn(self.conv(x)))
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
 
 
 class Bottleneck(nn.Module):
@@ -26,7 +38,7 @@ class Bottleneck(nn.Module):
   def __init__(self, ch_in, ch_out, shortcut=True, g=1, e=0.5):
     super().__init__()
     c_ = int(ch_in * e)  # hidden channels
-    self.cv1 = Conv(ch_out, c_, 1, 1)
+    self.cv1 = Conv(ch_in, c_, 1, 1)
     self.cv2 = Conv(c_, ch_out, 3, 1, g=g)
     self.add = shortcut and ch_in == ch_out
 
@@ -112,38 +124,34 @@ def check_version(current='0.0.0', minimum='0.0.0', name='version ', pinned=Fals
     else:
         return result
 
+class C3(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
 
-IN_CH = 3
-M = nn.Sequential(
-    Conv(IN_CH, 32, 3, 1),
-    Conv(32, 64, 3, 2),
-    Bottleneck(64, 64),
-    Conv(64, 128, 3, 2),
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
 
-    Bottleneck(128, 128),
-    Bottleneck(128, 128),
-    Conv(128, 256, 3, 2),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Bottleneck(256, 256),
-    Conv(256, 512, 3, 2),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Bottleneck(512, 512),
-    Conv(512, 1024, 3, 2),
-    Bottleneck(1024, 1024),
-    Bottleneck(1024, 1024),
-    Bottleneck(1024, 1024),
-    Bottleneck(1024, 1024),
-)
-print(M)
+class SPPF(nn.Module):
+    # Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        x = self.cv1(x)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')  # suppress torch 1.9.0 max_pool2d() warning
+            y1 = self.m(x)
+            y2 = self.m(y1)
+            return self.cv2(torch.cat([x, y1, y2, self.m(y2)], 1))
+
