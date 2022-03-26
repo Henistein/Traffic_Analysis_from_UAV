e9ee312bb7043a817feb8a107ecc989612540fdb
Henistein <henriquejesus20@gmail.com>
2022-03-26 23:52:52 +0000

    put inference as a class

--- yolov5/inference.py
+++ yolov5/inference.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import cv2
 import numpy as np
 from utils.general import non_max_suppression, letterbox, image_loader
@@ -8,87 +7,87 @@ from utils.conversions import coco2xyxy, scale_coords, normyolo2xyxy, clip_coord
 from models.yolo import Model
 
 
-def get_pred(model, img):
-  '''
-  returns prediction in numpy array
-  '''
-  model.eval()
-  imsize = 640
-  img, h, w = image_loader(img,imsize)
+class Inference:
+  def __init__(self, model, conf_thres=0.5, iou_thres=0.5, imsize=640, device=None):
+    if not device:
+      device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    self.device = torch.device(device)
+    self.model = model.to(self.device).eval()
 
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  img = img.to(device)
-  pred = model(img)[0]
-
-  pred = pred.cpu()
-
-  pred = non_max_suppression(pred, conf_thres=0.45)[0] # conf_thres is confidence thresold
-
-  if pred is not None:
-    # scale coords to match true image size
-    #pred = scale_coords((h, w), img[0].shape[1:], pred)
-    #pred = scale_coords((h, w), pred, img[0].shape[1:])
-    pred = scale_coords(img[0].shape[1:], pred, (h,w))
-  
-  return pred
-                
+    self.imsize = imsize
+    self.conf_thres = conf_thres
+    self.iou_thres = iou_thres
 
 
-def show_detections(detections, image, labels, ret=False, is_label=False):
-  img = image.copy()
-  for pred in detections:
-    x1,y1,x2,y2 = map(int, pred[:4])
+  def get_pred(self, img):
+    '''
+    returns prediction in numpy array
+    '''
+    img, h, w = image_loader(img,self.imsize)
+    img = img.to(self.device)
+    pred = self.model(img)[0]
 
-    c = pred[-1].item() # class
-    p = pred[-2].item() # confidence (proability)
+    pred = pred.cpu()
+    pred = non_max_suppression(pred, conf_thres=self.conf_thres, iou_thres=self.iou_thres)[0] # conf_thres is confidence thresold
 
-    start = (x1,y1)
-    end = (x2,y2)
-
-    if is_label:
-      pred_data = f'{labels[c]}'
-    else:
-      pred_data = f'{labels[c]} {str(p*100)[:5]}%'
-
-    color = (0,255,0)
-    img = cv2.rectangle(img, start, end, color)
-    img = cv2.putText(img, pred_data, (x1,y1+25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2, cv2.LINE_AA) 
-
-  # return img or show it
-  if ret: 
-    return img.copy()
-  else:
-    cv2.imshow('frame', img)
-    cv2.waitKey(0)
-
-
-def compute_stats(stats):
-
-  # compute metrics
-  stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
-  
-  if len(stats) and stats[0].any():
-    tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats)
-    ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
-    mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
-    nt = np.bincount(stats[3].astype(np.int64), minlength=80)  # number of targets per class
-    return mp, mr, map50, map
-  else:
-    nt = torch.zeros(1)
-    return nt
-
-
-def subjective(*stats, detections, labels, img):
-  mp, mr, map50, map = compute_stats([stats])
-  if map50 < 0.5:
-    print(f"mAP50: {map50}")
+    if pred is not None:
+      pred = scale_coords(img[0].shape[1:], pred, (h,w))
     
-    # get detections images and labels image with bb
-    det_img = show_detections(detections, img, ret=True)
-    l = torch.column_stack((labels[:, 1:], labels[:, 0]))
-    lab_img = show_detections(l, img, ret=True, is_label=True)
+    return pred
+                
+  @staticmethod
+  def attach_detections(detections, img, classnames, has_id=False, is_label=False):
+    for pred in detections:
+      x1,y1,x2,y2 = map(int, pred[:4])
 
-    # concatenate det_img and lab_img vertical
-    res = cv2.hconcat([det_img, lab_img])
-    cv2.imshow('frame', res)
-    cv2.waitKey(0)
+      c = int(pred[-1].item()) # class
+      p = pred[-2].item() # confidence (proability)
+
+      start = (x1,y1)
+      end = (x2,y2)
+
+      if has_id:
+        label = f'{pred[4]} {classnames[c]} {p:.2f}'
+      elif is_label:
+        label = f'{classnames[c]}' 
+      else:
+        label = f'{classnames[c]} {str(p*100)[:5]}%'
+
+      color = (0,255,0)
+      img = cv2.rectangle(img, start, end, color)
+      img = cv2.putText(img, label, (x1,y1+25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2, cv2.LINE_AA) 
+
+    return img.copy()
+
+
+  @staticmethod
+  def compute_stats(stats):
+    # compute metrics
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
+    
+    if len(stats) and stats[0].any():
+      tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats)
+      ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+      mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+      nt = np.bincount(stats[3].astype(np.int64), minlength=80)  # number of targets per class
+      return mp, mr, map50, map
+    else:
+      nt = torch.zeros(1)
+      return nt
+
+
+  @staticmethod
+  def subjective(*stats, detections, labels, img):
+    mp, mr, map50, map = Inference.compute_stats([stats])
+    if map50 < 0.5:
+      print(f"mAP50: {map50}")
+      
+      # get detections images and labels image with bb
+      det_img = Inference.attach_detections(detections, img, ret=True)
+      l = torch.column_stack((labels[:, 1:], labels[:, 0]))
+      lab_img = Inference.attach_detections(l, img, ret=True, is_label=True)
+
+      # concatenate det_img and lab_img vertical
+      res = cv2.hconcat([det_img, lab_img])
+      cv2.imshow('frame', res)
+      cv2.waitKey(0)
--- yolov5/main.py
+++ yolov5/main.py
@@ -6,7 +6,7 @@ from utils.conversions import xyxy2xywh
 from deep_sort.deep_sort import DeepSort
 from deep_sort.utils.parser import get_config
 
-from inference import get_pred, show_detections
+from inference import Inference
 from utils.conversions import scale_coords
 
 
@@ -65,68 +65,52 @@ def run_visdrone():
   run(model, video_path, classnames)
 
 def run_deepsort(model, video_path):
-  model.eval()
-  classnames = {model.names.index(k):k for k in model.names}
+  inf = Inference(model=model, device='cuda')
+  classnames = model.names
   cap = cv2.VideoCapture(video_path)
   # load deepsort
   cfg = get_config()
   cfg.merge_from_file("deep_sort/configs/deep_sort.yaml")
   deepsort = DeepSort('osnet_ibn_x1_0_MSMT17',
-                      torch.device('cuda'),
+                      inf.device,
                       max_dist=cfg.DEEPSORT.MAX_DIST,
                       max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                       max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                       )
   while cap.isOpened():
     ret, frame = cap.read()
-    # if frame is read correctly ret is True
     if not ret:
       print("Can't receive frame (stream end?). Exiting ...")
       break
 
-    orig = frame.copy()
-    #h,w = frame.shape[:2]
-    #print(h,w)
-    #frame = cv2.resize(frame, (640, 640), interpolation=cv2.INTER_AREA)
-    img = frame.copy()
-
     # inference
-    pred = get_pred(model, frame)
+    pred = inf.get_pred(frame)
 
-    xywhs = xyxy2xywh(pred[:, 0:4])
-    confs = pred[:, 4]
-    clss = pred[:, 5]
+    xywhs = xyxy2xywh(pred[:, 0:4]).cpu()
+    confs = pred[:, 4].cpu()
+    clss = pred[:, 5].cpu()
 
     # pass detections to deepsort
     outputs = deepsort.update(
-      xywhs.cpu(),
-      confs.cpu(),
-      clss.cpu(),
+      xywhs,
+      confs,
+      clss,
       frame.copy()
     )
+    
     if len(outputs) > 0:
-      for j, (output, conf) in enumerate(zip(outputs, confs)):
-        bboxes = output[0:4]
-        id = output[4]
-        cls = output[5]
+      # stack confs to outputs
+      min_dim = min(outputs.shape[0], confs.shape[0])
+      outputs = outputs[:min_dim]
+      confs = confs[:min_dim]
 
-        c = int(cls) # integer class
-        label = f'{id} {classnames[c]} {conf:.2f}'
+      confs = confs.unsqueeze(0).numpy().T
+      outputs = np.append(np.append(outputs[:, :5], confs, axis=1), outputs[:, 5].reshape(-1, 1), axis=1) # hack to be [bboxes, conf, class]
 
-        # scale  bboxes to original
-        #bboxes = scale_coords((640, 640), np.expand_dims(bboxes, 0).astype(np.float64), (h,w))[0].astype(np.int64)
-
-        start = bboxes[[0, 1]]
-        end = bboxes[[2, 3]]
-
-
-        color = (0,255,0)
-        img = cv2.rectangle(img, start, end, color)
-        img = cv2.putText(img, label, (bboxes[0],bboxes[1]+25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2, cv2.LINE_AA) 
-
-        cv2.imshow('frame', img)
-        if cv2.waitKey(1) == ord('q'):
-          break
+    frame = inf.attach_detections(outputs, frame, classnames, has_id=True)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+      break
   cap.release()
   cv2.destroyAllWindows()
 
@@ -135,7 +119,7 @@ if __name__ == '__main__':
   #run_coco()
 
   # run deepsort with yolo
-  weights = 'weights/visdrone100.pt'
+  weights = 'weights/visdrone.pt'
 
   model = torch.load(weights)['model'].float()
   model.to(torch.device('cuda'))
