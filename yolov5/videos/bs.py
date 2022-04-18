b9b60ac08b380b7965dec3ed503888d20191ce8d
Henistein <henriquejesus20@gmail.com>
2022-04-18 21:15:22 +0100

    update bs

--- yolov5/videos/bs.py
+++ yolov5/videos/bs.py
@@ -9,33 +9,70 @@ parser.add_argument('--input', type=str, help='Path to a video or a sequence of 
 parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
 args = parser.parse_args()
 if args.algo == 'MOG2':
-    backSub = cv.createBackgroundSubtractorMOG2(history=50, dist2Threshold=300)
+    backSub = cv.createBackgroundSubtractorMOG2(history=50)
 else:
-    backSub = cv.createBackgroundSubtractorKNN(history=50, dist2Threshold=300)
+    backSub = cv.createBackgroundSubtractorKNN(history=250, dist2Threshold=250)
 capture = cv.VideoCapture(cv.samples.findFileOrKeep(args.input))
 if not capture.isOpened():
     print('Unable to open: ' + args.input)
     exit(0)
+
+def histogram_equalization(img):
+    # Perform Histogram Equalization to counter illumination change problem
+    # convert the color scheme to YCrCb which separate the intensity/brightness information of image separately unlike rgb
+    img_ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCR_CB)
+    
+    # perform histogram equalization on the Y-channel of this image
+    # for this, we use CLAGE's algorithm (that performs piecewise histogram equalization)
+    clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(2,2))
+    img_ycrcb[:,:,0] = clahe.apply(img_ycrcb[:,:,0])
+    
+    # convert back to bgr
+    img = cv.cvtColor(img_ycrcb, cv.COLOR_YCR_CB2BGR)
+    return img
+
+def adaptive_thresholding(img):
+    channels = []
+    for channel in cv.split(img):
+        channel_bg = cv.medianBlur(cv.dilate(channel, np.ones((3,3))), 25)
+        channel_edges = cv.absdiff(channel, channel_bg)
+        channel_edges = cv.normalize(channel_edges, dst = None, norm_type = cv.NORM_MINMAX, alpha = 0, beta = 255)
+        channels.append(channel_edges)
+    return cv.merge(channels)
+
 while True:
     ret, frame = capture.read()
     if frame is None:
         break
     
-    fgMask = backSub.apply(frame)
+
+    #histogram_equalization(frame)
+    adaptive_thresholding(frame)
+    frame = backSub.apply(frame)
 
     # apply image dilation
     kernel = np.ones((3, 3), np.uint8)
     kernel2 = np.ones((5, 5), np.uint8)
-    fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN, kernel)
-    fgMask = cv.morphologyEx(fgMask, cv.MORPH_CLOSE, kernel2)
-    fgMask = cv.medianBlur(fgMask,5)
-    #_, fgMask = cv.threshold(fgMask, 127, 255, 0)
+    #frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+    #frame = cv.morphologyEx(frame, cv.MORPH_CLOSE, kernel2)
+    #frame = cv.medianBlur(frame,5)
+
+    # dynamic bgs
+    frame = cv.erode(frame, kernel)
+    frame = cv.erode(frame, kernel)
+    frame = cv.dilate(frame, kernel)
+    frame = cv.dilate(frame, kernel)
+
+    frame = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
+    frame = cv.morphologyEx(frame, cv.MORPH_CLOSE, kernel2)
+    frame = cv.medianBlur(frame, 7)
     
 
     # contours
-    #contours, hierarchy = cv.findContours(fgMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    #fgMask = cv.cvtColor(fgMask, cv.COLOR_GRAY2BGR)
-    #fgMask = cv.drawContours(fgMask, contours, -1, (0,255,0), 3)
+    #contours, hierarchy = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    #frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+    #frame = cv.drawContours(frame, contours, -1, (0,255,0), 3)
+    #print(contours)
 
      
     
@@ -45,7 +82,7 @@ while True:
     
     
     #cv.imshow('Frame', frame)
-    cv.imshow('FG Mask', fgMask)
+    cv.imshow('FG Mask', frame)
     
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
